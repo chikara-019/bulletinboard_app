562b675a3cf0c9b3f479953c6fe65ba4fdafcaec
Chikara Hamada <norg.hamada@gmail.com>
2024-02-05 13:24:32 +0900

    post->indexリダイレクトいけてる??

--- app/src/index.php
+++ app/src/index.php
@@ -2,6 +2,7 @@
 
     require_once 'database.php';
     require_once 'functions.php';
+    require_once 'post.php';
 
     $dbHandler = new Database('run-php-db', 'bbs_yt', 'root', 'root');
     $pdo = $dbHandler->getpdo();
@@ -28,50 +29,6 @@
     $comment_array = $pdo->query($sql);//sql文をqueryを使って問い合わせができる
     
 
-
-    //フォームを打ち込んだ時
-    if(!empty($_POST["submitButton"])){
-
-        $postdate = date("Y-m-d H:i:s");
-
-        //名前チェック
-        if(empty($_POST["username"])){
-            echo "名前を入力してください。";
-        }elseif(!preg_match('/^.{1,30}$/', $_POST["username"])){
-            echo "名前は30文字以内で入力してください。";
-        }
-
-        //コメントチェック
-        if(empty($_POST["comment"])){
-            echo "コメントを入力してください。";
-        }elseif(!preg_match('/^.{1,200}$/', $_POST["comment"])){
-            echo "コメントは200文字以内で入力してください。";
-
-        }else{
-
-
-            try{
-                $stmt = $pdo->prepare("INSERT INTO bbs_table (username, title, comment, postdate) VALUES(:username, :title, :comment, :postdate)");
-                $stmt->bindParam(':username', $_POST['username'], PDO::PARAM_STR);
-                $stmt->bindParam(':title', $_POST['title'], PDO::PARAM_STR);
-
-                $stmt->bindParam(':comment', $_POST['comment'], PDO::PARAM_STR);
-                $stmt->bindParam(':postdate', $postdate, PDO::PARAM_STR);
-        
-                $stmt->execute();
-                
-                echo "投稿完了。";
-
-            }catch(PDOException $e){
-                //echo "投稿に失敗しました。". $e->getMessage();
-                header('Location: post.php' . urlencode("投稿に失敗しました。" . $e->getMessage()));
-                exit;
-            }
-        
-
-        }
-
-    }
 
 
 
--- app/src/post.php
+++ app/src/post.php
@@ -7,27 +7,48 @@ date_default_timezone_set("Asia/Tokyo");
 
 
 
-try {
-    $pdo = new PDO('mysql:host=localhost;dbname=your_database', 'username', 'password');
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-    $stmt = $pdo->prepare("INSERT INTO  bbs_table(username, title, body, postdate) VALUES(:username, :title, :comment, now())");
-    $stmt->bindParam(':username', $username);
-    $stmt->bindParam(':title', $title);
-    $stmt->bindParam(':comment', $comment);
-    $stmt->execute();
-
-    echo "投稿が保存されました。";
-
-    header('index.php');
-    exit();
-    
-} catch(PDOException $e) {
-    // エラーメッセージの生成
-    $errorMessage = "データ保存中にエラーが発生しました: " . $e->getMessage();
-
-    echo $errorMessage;
-    //echo $e->getMessage();
 
 
-}
+    //フォームを打ち込んだ時
+    if(!empty($_POST["submitButton"])){
+
+        $postdate = date("Y-m-d H:i:s");
+
+        //名前チェック
+        if(empty($_POST["username"])){
+            echo "名前を入力してください。";
+        }elseif(!preg_match('/^.{1,30}$/', $_POST["username"])){
+            echo "名前は30文字以内で入力してください。";
+        }
+
+        //コメントチェック
+        if(empty($_POST["comment"])){
+            echo "コメントを入力してください。";
+        }elseif(!preg_match('/^.{1,200}$/', $_POST["comment"])){
+            echo "コメントは200文字以内で入力してください。";
+
+        }else{
+
+
+            try{
+                $stmt = $pdo->prepare("INSERT INTO bbs_table (username, title, comment, postdate) VALUES(:username, :title, :comment, :postdate)");
+                $stmt->bindParam(':username', $_POST['username'], PDO::PARAM_STR);
+                $stmt->bindParam(':title', $_POST['title'], PDO::PARAM_STR);
+
+                $stmt->bindParam(':comment', $_POST['comment'], PDO::PARAM_STR);
+                $stmt->bindParam(':postdate', $postdate, PDO::PARAM_STR);
+        
+                $stmt->execute();
+                
+                echo "投稿完了。";
+
+            }catch(PDOException $e){
+                //echo "投稿に失敗しました。". $e->getMessage();
+                header('Location: index.php' . urlencode("投稿に失敗しました。" . $e->getMessage()));
+                exit;
+            }
+        
+
+        }
+
+    }
